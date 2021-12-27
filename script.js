9bea2a7ec5634f2d89ff6bb6bf1af2db6bd7be6e
Patrycja <pwbanaszuk@gmail.com>
2021-12-27 21:41:24 +0000

    added a second solution of the problem to the script.js as a comment

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const input = document.querySelector('input')
 const drinksList = document.querySelectorAll('li')
 
-
 const drinksSearch = () => {
 	drinksList.forEach(drink => {
 		if (drink.textContent.toLowerCase().indexOf(input.value.toLowerCase()) !== -1) {
@@ -11,5 +10,19 @@ const drinksSearch = () => {
 		}
 	})
 }
+
+//solution with RegExp below:
+
+// const drinksSearch = () => {
+// 	drinksList.forEach(drink => {
+// 		const match = new RegExp(input.value, 'i').test(drink.textContent)
+
+// 		if (!match) {
+// 			drink.style.display = 'none'
+// 		} else {
+// 			drink.style.display = 'flex'
+// 		}
+// 	})
+// }
 
 input.addEventListener('keyup', drinksSearch)
